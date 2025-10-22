ad9b05a38241aeb0f51503994785fd357eb747f6
fauziramdani <fauziramdani234@gmail.com>
2025-10-22 17:34:30 +0700

    fix : delete personal chat and adjust layout chatlist

--- src/app/api/chat-list/route.ts
+++ src/app/api/chat-list/route.ts
@@ -1,6 +1,6 @@
 import { chatController } from "@/controllers/chatController";
 import { authOptions } from "@/lib/auth";
-import { decrypt } from "@/lib/encryptor";
+import { decryptFromUrl } from "@/lib/encryptor";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
@@ -12,7 +12,7 @@ export async function GET(req: Request) {
     throw new Error("userId query parameter is missing");
       }
     const decodedId = decodeURIComponent(userId);
-    const decryptUserId = await decrypt(decodedId);
+    const decryptUserId = await decryptFromUrl(decodedId);
     const data = await chatController.getChatList(Number(decryptUserId));
     return NextResponse.json(data);
   } catch (error) {
@@ -30,6 +30,23 @@ export async function POST(req: Request) {
     }
     const body = await req.json();
     const message = await chatController.createChatList(body.userId);
+    return NextResponse.json(message);
+  } catch (error) {
+    console.error("POST /api/chat error:", error);
+    return NextResponse.json({ error: "Failed to send message" }, { status: 500 });
+  }
+}
+
+export async function DELETE(req: Request) {
+  try {
+    const session = await getServerSession(authOptions);
+
+    if (!session || !session.user) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+    const body = await req.json();
+    const id = await decryptFromUrl(body.id);
+    const message = await chatController.deletePersonalChat(Number(id));
     return NextResponse.json(message);
   } catch (error) {
     console.error("POST /api/chat error:", error);
